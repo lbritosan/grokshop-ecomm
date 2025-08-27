460e0404e899dd919b00cf2641b53fe616ca6964
lbritosan <lbritosan@gmail.com>
2025-08-27 16:26:41 -0300

    Initial

---
+++ src/main/java/com/lbritosan/grokshop/GrokshopApplication.java
@@ -0,0 +1,13 @@
+package com.lbritosan.grokshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrokshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrokshopApplication.class, args);
+	}
+
+}
